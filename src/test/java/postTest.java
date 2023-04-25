655730fab96b4f0a6f1fd9ee035852523982cc2e
neto study <kseniyalazare@yandex.ru>
2023-04-25 19:57:12 +0300

    initial commit

---
+++ src/test/java/postTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class postTest {
+    @Test
+    void shouldPost() {
+
+        // Given - When - Then
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("netology")
+                .when()
+                .post("/post")
+                .then()
+                .log()
+                .body()
+                .statusCode(200)
+                .body("data", equalTo("netology"))
+                ;
+    }
+}
